b140c1a02b3f2f1968ddf1fd024b9511c773d013
Michael <mich561d@hotmail.com>
2019-02-13 12:42:21 +0100

    even more new things

--- src/main/webapp/Assets/JavaScripts/javascript.js
+++ src/main/webapp/Assets/JavaScripts/javascript.js
@@ -5,18 +5,19 @@ var clear = document.getElementById("clear");
 for (var i = 0; i < operations.length; i++) {
     operations[i].addEventListener('click', function (event) {
         var operation = switcherino(operations[i]);
-        var n1 = document.getElementById("firstNumber");
-        var n2 = document.getElementById("secondNumber");
-        
-        fetch(`calculator?operation=${operation}&n1=${n1}&n2=${n2}`).then(res => res.text())
-                .then(d => {
-                    result.innerHTML = d;
-                });
+        var n1 = document.getElementById("firstNumber").value;
+        var n2 = document.getElementById("secondNumber").value;
+
+        fetch(`calculator?operation=${operation}&n1=${n1}&n2=${n2}`)
+                .then(res => res.text())
+                .then(d => { result.innerHTML = d; });
     });
 }
 
 clear.addEventListener('click', function (event) {
     result.innerHTML = "";
+    document.getElementById("firstNumber").innerHTML = "";
+    document.getElementById("secondNumber").innerHTML = "";
 });
 
 function switcherino(operation) {
